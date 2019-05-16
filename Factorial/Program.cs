2a85855945719635774d2bb860b9c6a46bba67dc
Lilo <blabla1234560@gmail.com>
2019-05-16 23:37:08 +0200

    first

--- Factorial/Program.cs
+++ Factorial/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿//Calculating factorial in for loop and recursion
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +12,31 @@ namespace Factorial
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter the n value: ");
+            var n = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($" Facotrial of {n} is {ForLoop(n)} using ForLoop ");
+            Console.WriteLine($" Facotrial of {n} is {Recursion(n)} using Recursion ");
+        }
+
+        private static int ForLoop(int n)
+        {
+            var factorial = 1;
+            for (int i = n; i >= 1; i--)
+            {
+                factorial *= i;
+            }
+
+            return factorial;
+        }
+
+
+        private static int Recursion(int n)
+        {
+            if (n == 1)
+                return 1;
+
+            return n * Recursion(n - 1);
         }
     }
 }
